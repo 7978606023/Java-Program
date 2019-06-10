c2ae9da5703cc4b8f3baf28a1d133ed7b81ba019
Your Name <you@example.com>
2019-06-10 13:01:32 +0530

    code modified

--- Assignment/src/com/wbl/imp/DuplicateArray.java
+++ Assignment/src/com/wbl/imp/DuplicateArray.java
@@ -5,15 +5,25 @@ import java.util.Set;
 
 public class DuplicateArray{
 	
-	public void removeDup(int []arr) {
+	public static int[] removeDup(int[] arr) {
+
 		int count = 0;
-	
-		Set hs = new HashSet();
+
+		Set st = new HashSet();
 		
-		for(int i=0;i<arr.length;i++) {
-			if(hs.add(arr[i])) {
-				System.out.println(arr[i]);
+		int []temp = new int[arr.length];
+		
+		if (arr.length == 0) {
+			System.out.println("We can't perform removal operation of duplicates elements");
+		} 
+		else {
+			for (int i = 0; i < arr.length; i++) {
+				if (st.add(arr[i])) {
+					temp[count] = arr[i];
+					count++;
+				}
 			}
 		}
+		return temp;
 	}
 }
